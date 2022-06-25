39ef4f9c560c66950006f918fdc4a20a91dbbe0e
suhelmulla <suhel.mulla@emerson.com>
2022-06-26 01:29:02 +0530

    Decide whether a number is even or odd
    Decide whether a number is even or odd.

--- even_odd_number/even_odd/even_odd/even_odd.cpp
+++ even_odd_number/even_odd/even_odd/even_odd.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+    int a = 0;
+
+    printf("\n Enter a number : ");
+    scanf_s("%d", &a);
+
+    // Below method is used because modulus method is costly (requires more power and time to execucte)
+    if ((a & 0x01) == 1)
+        printf("\n The number is odd.");
+    else
+        printf("\n The number is even.");
+
+    printf("\n");
+
     return 0;
 }
