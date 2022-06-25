cebdcb175a9e9d0cc6e9ae7eb02243b714d3b7a4
suhelmulla <suhel.mulla@emerson.com>
2022-06-26 02:04:35 +0530

    Find set bits in a number
    Find set bits in a number

--- even_odd_number/even_odd/even_odd/even_odd.cpp
+++ even_odd_number/even_odd/even_odd/even_odd.cpp
@@ -1,6 +1,19 @@
+/* Find the number of set bits in a number. */
+
 #include<stdio.h>
 
 int main()
 {
+    int num = 0;
+    int count = 0;
+    scanf_s("%d", &num);
+
+    while (num != 0)
+    {
+        num = num & (num - 1);
+        count++;
+    }
+    printf("%d", count);
+
     return 0;
 }
