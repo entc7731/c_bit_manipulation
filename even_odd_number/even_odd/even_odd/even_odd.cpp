3fb57b517fa3874744e942a12cbc19a7a599da7a
suhelmulla <suhel.mulla@emerson.com>
2022-06-26 01:49:40 +0530

    check whether ith bit is set in a given number
    check whether ith bit is set in a given number

--- even_odd_number/even_odd/even_odd/even_odd.cpp
+++ even_odd_number/even_odd/even_odd/even_odd.cpp
@@ -1,6 +1,16 @@
+/* Is ith bit set in a given number? */
+
 #include<stdio.h>
 
 int main()
 {
+    int num, i;
+    scanf_s("%d %d",&num, &i);
+
+    if (num & (1 << (i - 1)))
+        printf("\n YES");
+    else
+        printf("\n NO");
+
     return 0;
 }
